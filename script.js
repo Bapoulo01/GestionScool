cd12d45d55eaeebb4753fb7515ea53ef59a803b4
Rokhayatoure <rokhayatoure2000@gmail.com>
2024-12-15 20:12:48 +0000

    notre v2

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-const sidebar = document.querySelector(".sidebar");
-const sidebarToggler = document.querySelector(".sidebar-toggler");
-
-sidebarToggler.addEventListener("click",() =>{
+ const sidebar = document.querySelector(".sidebar");
+ const sidebarToggler = document.querySelector(".sidebar-toggler");
+// sidebarToggler represente le classe name du fleche
+ sidebarToggler.addEventListener("click",() =>{
+    // sa s'est une ecouteur d'evenement 
     sidebar.classList.toggle("collapsed");  //remplace sidebar par collapsed
+    // alert('ok');
+    
 
 })
+
