79a637e1c4222384956a57443bf1987fdf8ac10c
Bapoulo01 <amdoux2.0@gmail.com>
2024-12-20 18:00:45 +0000

    Demo_V1

--- src/script.js
+++ src/script.js
@@ -3,19 +3,44 @@ const sidebarToggler = document.querySelector(".sidebar-toggler");
 const tBodyCours = document.querySelector("#tBodyCours")
 
 let cours=[];
+let professeurs = [];
+let semestres = [];
 document.addEventListener("DOMContentLoaded",async(event)=>{
 
-    let datas = await findAllCours();
-    cours = [...datas];
+  await loadData();
+    // let datas = await findAllCours();
+    // cours = [...datas];
     init();
-    console.log(cours);
+    // console.log(cours);
 
 
     //fonction qui retourne les cours
-async function findAllCours(){
-    let response = await fetch(`http://localhost:3000/Cours`);    //fetch fct async
-    let datas = await response.json();
-    return datas;
+// async function findAllCours(){
+//     let response = await fetch(`http://localhost:3000/Cours`);    //fetch fct async
+//     let datas = await response.json();
+//     return datas;
+// }
+
+// Fonction pour charger toutes les données nécessaires
+async function loadData() {
+  // Récupérer les cours, professeurs et semestres
+  const [coursData, profData, semestreData] = await Promise.all([
+    fetch("http://localhost:3000/Cours").then((res) => res.json()),
+    fetch("http://localhost:3000/Professeurs").then((res) => res.json()),
+    fetch("http://localhost:3000/Semestres").then((res) => res.json()),
+  ]);
+
+  // Sauvegarder les données globales
+  cours = [...coursData];
+  professeurs = [...profData];
+  semestres = [...semestreData];
+
+  // Enrichir les cours avec les informations des professeurs et des semestres
+  cours = cours.map((c) => ({
+    ...c,
+    professeur: professeurs.find((p) => p.idProf === c.idProf)?.nom || "Inconnu",
+    semestre: semestres.find((s) => s.idSemestre === c.idSemestre)?.nom || "Inconnu",
+  }));
 }
 
 function init() {
